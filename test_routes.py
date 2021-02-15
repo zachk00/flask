729b69b424480de9e6b3bf5ec20ddb2996e77fa9
Zachary Kerr <zacharykerr@ufl.edu>
2021-02-15 14:37:28 -0500

    Add files via upload

---
+++ test_routes.py
@@ -0,0 +1,16 @@
+from flask import Flask
+import json
+from flask_.handlers.routes import configure_routes
+
+
+  
+def test_index():
+    app = Flask(__name__)
+    configure_routes(app)
+    client = app.test_client()
+    url = '/'
+
+    response = client.get(url)
+    assert response.get_data() == b'Hello, World!'
+    assert response.status_code == 200
+ 
