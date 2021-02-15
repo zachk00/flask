157c27c416faab0c994ec3ae1c3cca4b33fd3a20
Zachary Kerr <zacharykerr@ufl.edu>
2021-02-15 14:36:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template
-
+from flask import Flask
+from flask_.handlers.routes import configure_routes
 
 app = Flask(__name__)
 
-@app.route('/')
+configure_routes(app)
 
-def index():
-    return render_template("index.html")
+if __name__ == '__main__':
+    app.run()
