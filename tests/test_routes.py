3e4bcc75ba83404db7f8dee81b30534a6eb5ae75
Zachary Kerr <zacharykerr@ufl.edu>
2021-02-16 18:44:27 -0500

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,16 +1,32 @@
 from flask import Flask
 import json
-from ..handlers.routes import configure_routes
+from flask_.handlers.routes import configure_routes
 
 
   
-def test_index():
-    app = Flask(__name__)
+def test_home():
+    app = Flask(__name__, template_folder='../templates')
     configure_routes(app)
     client = app.test_client()
     url = '/'
 
-    response = client.get(url)
-    assert response.get_data() == b'Hello World!'
+    response = client.get(url, content_type='html/text')
+    
     assert response.status_code == 200
+    
  
+def test_blog():
+    app = Flask(__name__, template_folder='../templates')
+    configure_routes(app)
+    client = app.test_client()
+    url = '/blog'
+    response = client.get(url)
+    assert response.get_data() == b'blog'
+    
+def test_blog_content():
+    app = Flask(__name__, template_folder='../templates')
+    configure_routes(app)
+    client = app.test_client()
+    url = '/blog'
+    response = client.get(url)
+    assert response.get_data() != b'blug'
